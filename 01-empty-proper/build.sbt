baaf8b83ccea9bd236b82ccf0b0ebc79b4660e11
Marko Elezovic <marko@mentat-labs.com>
2016-02-23 14:21:23 +0100

    Added empty examples

---
+++ 01-empty-proper/build.sbt
@@ -0,0 +1,5 @@
+organization := "org.hrscala"
+name := "empty-proper"
+version := "0.0.1"
+
+scalaVersion := "2.11.7"
---
+++ 01-empty-proper/src/main/scala/org/hrscala/sbt/Main.scala
@@ -0,0 +1,5 @@
+package org.hrscala.sbt
+
+object Main extends App {
+  println("Hello world via Scala " + scala.util.Properties.versionNumberString)
+}
