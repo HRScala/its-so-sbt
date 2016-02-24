cf3a6cca8395c56545a8aa00828f23b147a26610
Davor Stimac <davor.stimac@gmail.com>
2016-02-24 14:19:37 +0100

    authorship removed from 30-sbt-multi-project-build

--- 30-sbt-multi-project-build/api/src/main/scala/org/hrscala/sbt/BaseTrait.scala
+++ 30-sbt-multi-project-build/api/src/main/scala/org/hrscala/sbt/BaseTrait.scala
@@ -1,8 +1,5 @@
 package org.hrscala.sbt
 
-/**
-  * Created by davor on 2/24/16.
-  */
 trait BaseTrait {
   def implementMe: String
 }
--- 30-sbt-multi-project-build/core/src/main/scala/org/hrscala/sbt/Hello.scala
+++ 30-sbt-multi-project-build/core/src/main/scala/org/hrscala/sbt/Hello.scala
@@ -1,8 +1,5 @@
 package org.hrscala.sbt
 
-/**
-  * Created by davor on 2/24/16.
-  */
 object Hello extends App with BaseTrait {
   override def implementMe: String = "\nHello, I have implemented method 'implementMe' from BaseTrait!\n"
 
